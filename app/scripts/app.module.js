3adf0d03ae1a78d9257ec3a1783581be102e1aea
Christopher J Lyth <christopher.lyth@gmail.com>
2016-06-10 06:35:49 -0500

    initial auth working

--- app/scripts/app.module.js
+++ app/scripts/app.module.js
@@ -3,36 +3,71 @@
   'use strict';
 
   angular.module('piPlant', [
-    'ngRoute'
-  ]).controller('ParticleAuthTokenController', [
-    '$scope', '$routeParams', '$location', '$rootScope',
-    function($scope, $routeParams, $location, $rootScope) {
-      $scope.name = 'ParticleAuthTokenController';
-      if (!$rootScope.tokens) {
-        $rootScope.tokens = {};
+    'ngRoute',
+    'firebase'
+  ]).factory('particleService', [
+    '$rootScope',
+    function($rootScope) {
+      return {
+        isReady: function isReady() {
+          return Boolean($rootScope.tokens && $rootScope.tokens.particle);
+        }
+      };
+    }])
+    .controller('HeaderController',
+      ['$scope', '$routeParams', 'particleService', '$firebaseAuth',
+        function($scope, $routeParams, particleService, $firebaseAuth) {
+          $scope.checkParticle = function checkParticle() {
+            var auth = $firebaseAuth();
+
+            // login with Facebook
+            auth.$signInWithPopup('google').then(function(firebaseUser) {
+              console.log('Signed in as:', firebaseUser);
+            }).catch(function(error) {
+              console.log('Authentication failed:', error);
+            });
+            // console.log('$firebaseAuth', $firebaseAuth);
+            // console.log('particleService.isReady:', particleService.isReady());
+          };
+        }
+      ])
+    .controller('ParticleAuthTokenController',
+      ['$scope', '$routeParams', '$location', '$rootScope',
+        function($scope, $routeParams, $location, $rootScope) {
+          $scope.name = 'ParticleAuthTokenController';
+          if (!$rootScope.tokens) {
+            $rootScope.tokens = {};
+          }
+          if ($location.hash() && $location.hash().startsWith('token=')) {
+            $rootScope.tokens.particle = $location.hash().split('=')[1];
+            $location.path('/').replace();
+            $location.hash('');
+          } else {
+            console.log('Error parsing auth token');
+          }
+        }
+      ])
+    .controller('MainController',
+      ['$scope', '$routeParams', '$location', '$rootScope', '$firebaseObject',
+        function($scope, $route, $routeParams, $location, $firebaseObject) {
+          var ref = firebase.database().ref().child('data');
+          var syncObject = $firebaseObject(ref);
+          syncObject.$bindTo($scope, 'data');
+
+          $scope.$route = $route;
+          $scope.$location = $location;
+          $scope.$routeParams = $routeParams;
+        }
+      ])
+    .config(['$locationProvider', '$routeProvider',
+      function($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
+        $routeProvider.when('/particle/login', {
+          templateUrl: 'scripts/particle-login/particle-login.template.html'
+        }).when('/particle/auth/token', {
+          controller: 'ParticleAuthTokenController',
+          template: '<h1>TOKEN</h1>'
+        }).otherwise('/login');
       }
-      if ($location.hash() && $location.hash().startsWith('token=')) {
-        $rootScope.tokens.particle = $location.hash().split('=')[1];
-        $location.path('/').replace();
-        $location.hash('');
-      } else {
-        console.log('Error parsing auth token');
-      }
-    }]).controller('MainController', [
-      '$scope', '$routeParams', '$location', '$rootScope',
-      function($scope, $route, $routeParams, $location) {
-        $scope.$route = $route;
-        $scope.$location = $location;
-        $scope.$routeParams = $routeParams;
-      }
-    ]).config(['$locationProvider', '$routeProvider',
-    function($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix('!');
-      $routeProvider.when('/particle/login', {
-        templateUrl: 'scripts/particle-login/particle-login.template.html'
-      }).when('/particle/auth/token', {
-        controller: 'ParticleAuthTokenController',
-        template: '<h1>TOKEN</h1>'
-      }).otherwise('/login');
-    }]);
+    ]);
 })();
