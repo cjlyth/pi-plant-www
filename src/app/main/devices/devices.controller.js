2c2dede072abdbd117fb3b4789ab0007cfe9b64c
Chris Lyth <cjlyth@gmail.com>
2016-06-28 21:09:31 -0500

    Took the button off in favor of a second section

--- src/app/main/devices/devices.controller.js
+++ src/app/main/devices/devices.controller.js
@@ -6,22 +6,20 @@
     .controller('DevicesController', DevicesController);
 
   /** @ngInject */
-  function DevicesController($log, $scope, firebaseAuth, resolvedUser, $firebaseObject, particleApi) {
+  function DevicesController($log, $scope, resolvedUser, $firebaseObject, particleApi) {
     var devices = this;
 
     devices.getDevices = getDevices;
-    devices.toggleLed = toggleLed;
     devices.accountInfo = {};
     devices.particleDeviceList = [];
     devices.particleAccessTokenValid = false;
+
     var ref = firebase.database().ref().child("users").child(resolvedUser.uid);
     var accountInfo = $firebaseObject(ref);
     accountInfo.$bindTo($scope, "devices.accountInfo");
 
     $scope.$watch('devices.accountInfo.particleAccessToken', devices.getDevices);
-    
-    
-    
+
     activate();
     function activate() {
       $log.debug('devices activate', particleApi);
@@ -31,13 +29,8 @@
         return;
       }
       $log.debug('attempting to list devices', particleAccessToken);
-      var devicesPr = particleApi.listDevices({ auth: particleAccessToken });
-
-      return devicesPr.then(
-          getDevicesComplete,
-          getDevicesFailed
-      );
-
+      return particleApi.listDevices({ auth: particleAccessToken })
+          .then(getDevicesComplete, getDevicesFailed);
     }
     function getDevicesComplete(response) {
       $scope.$apply(function(){
@@ -51,23 +44,6 @@
         devices.particleAccessTokenValid = false;
         devices.particleDeviceList = [];
       });
-    }
-    
-    function toggleLed(deviceId, state){
-      $log.debug('toggleLed', deviceId)
-      var fnPr = particleApi.callFunction({
-        auth: devices.accountInfo.particleAccessToken,
-        deviceId: deviceId,
-        argument: state ? 'on' : 'off',
-        name: 'toggleState' });
-
-      fnPr.then(
-          function(data) {
-            devices.accountInfo.particles[deviceId].selected = !!data.body.return_value;
-            console.log('Function called succesfully:', data);
-          }, function(err) {
-            console.log('An error occurred:', err);
-          });
     }
   }
 })();
